4c130ff9cb6e09b2abed337239e760c1ad023fa0
PanditUtkarsh <utkp200@gmail.com>
2023-07-29 21:33:18 +0530

    first commit

---
+++ age.java
@@ -0,0 +1,8 @@
+public class age {
+
+    public static void main(String[] args) {
+        int age = 19;
+        String res = age >=18 ? "ELIGIBLE TO VOTE" : "NO ELIGIBLE TO VOTE";
+        System.out.println(res);
+    }
+}
