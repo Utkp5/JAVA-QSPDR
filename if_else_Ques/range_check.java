ef97042c08e81e2a8476334ec9fa631795409049
PanditUtkarsh <utkp200@gmail.com>
2023-07-31 21:35:47 +0530

    if else

---
+++ if_else_Ques/range_check.java
@@ -0,0 +1,31 @@
+package if_else_Ques;
+
+import java.util.Scanner;
+
+public class range_check {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("ENTER THE NUMBER : "); 
+        int num = sc.nextInt();
+
+        if (num==0) {
+            System.out.println("NUMBER IS EQUAL TO 0");
+        }
+        else if (num < 0) {
+             System.out.println("NUMBER IS LESS THAN 0");
+        }
+        else if (num > 0 && num <= 100 ) {
+             System.out.println("NUMBER IS BETWEEN 0 TO 100");
+        }
+        else if (num > 100 && num <= 200) {
+             System.out.println("NUMBER IS BETWEEN 100 TO 200");
+        }
+        else  {
+             System.out.println("NUMBER IS ABOVE 200");
+        }
+        
+
+        sc.close();
+    }    
+}
