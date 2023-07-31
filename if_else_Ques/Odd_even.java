5e1b8102e185f69b452b1175bf0c5dbbc118893b
PanditUtkarsh <utkp200@gmail.com>
2023-07-31 20:07:11 +0530

    if else

--- if_else_Ques/Odd_even.java
+++ if_else_Ques/Odd_even.java
@@ -1,2 +1,24 @@
-// Q WAP to take a no from user and check whether the no is odd even
+//  Q WAP to take a no from user and check whether the no is odd even
 
+import java.util.Scanner;
+
+public class Odd_even {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("ENTER YOUR NUMBER : ");
+        int num = sc.nextInt();
+
+        if (num%2==0) {
+            System.out.println("EVEN NUMBER");            
+        }
+        else
+        {
+            System.out.println("ODD NUMBER");
+        }
+        
+
+    }
+    
+}
