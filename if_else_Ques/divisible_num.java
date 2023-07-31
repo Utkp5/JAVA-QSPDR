59bf1fe0f85d7681972c441b5fe1552d79c0ae0b
PanditUtkarsh <utkp200@gmail.com>
2023-07-31 21:42:58 +0530

    if else

---
+++ if_else_Ques/divisible_num.java
@@ -0,0 +1,29 @@
+package if_else_Ques;
+
+import java.util.Scanner;
+
+public class divisible_num {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("ENTER THE NUMBER : "); 
+        int num = sc.nextInt();
+
+        if (num%3==0 && num%2==0) {
+            System.out.println("Rahat and shivi");
+        }
+        else if (num%3==0) {
+             System.out.println("shivi");
+        }
+        else if (num%2==0) {
+             System.out.println("Rahat");
+        }        
+        else  {
+             System.out.println("Devdas");
+        }
+
+
+        sc.close();
+
+    }        
+}
