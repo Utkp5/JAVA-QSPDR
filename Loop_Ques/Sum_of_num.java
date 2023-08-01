a0fed4eedcb3f769a3c1692ef97e024f95d9dfba
PanditUtkarsh <utkp200@gmail.com>
2023-08-01 21:20:50 +0530

    loop ques

--- Loop_Ques/Sum_of_num.java
+++ Loop_Ques/Sum_of_num.java
@@ -1,5 +1,26 @@
 package Loop_Ques;
+import java.util.Scanner;
+
+// Question :-  WAP TO FIND THE SUM OF DIGIT OF A NUMBER
+//like eg(8976 : - 8+9+7+6=30)
 
 public class Sum_of_num {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("ENTER A NUMBER : "); 
+        int num = sc.nextInt();
+        
+        int i = 1,temp = num,sum = 0,tempval = 0;
+
+        while (i<=temp) {
+            tempval = temp%10;
+            sum = sum+tempval;
+            temp = temp/10;
+            i++;
+        }
+        System.out.print("SUM OF " + num + " IS : " + sum); 
+
+        sc.close();
+    }
 }
