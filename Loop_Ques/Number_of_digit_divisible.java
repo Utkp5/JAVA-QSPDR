bc4029b3473aa0c918ca88aefa22ae0cbcbe3c63
PanditUtkarsh <utkp200@gmail.com>
2023-08-02 21:02:22 +0530

    loop ques

--- Loop_Ques/Number_of_digit_divisible.java
+++ Loop_Ques/Number_of_digit_divisible.java
@@ -1,11 +1,11 @@
 package Loop_Ques;
 import java.util.Scanner;
 
-// Question :-  WAP TO FIND THE NUMBER OF ODD DIGIT IN A NUMBER
-//like eg(46731 : - 7,3,1 = 3)
+// Question :-  WAP TO FIND THE NUMBER OF DIGIT DIVISIBLE BY 3 
+//like eg(46731 : - 6,3 = 2)
 
 
-public class Number_of_odd_digit {
+public class Number_of_digit_divisible {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -18,13 +18,13 @@ public class Number_of_odd_digit {
                 last_digit = num % 10;
                 num = num / 10;
 
-                if (!(last_digit%2==0)) {
+                if ((last_digit%3==0)) {
                     count++;
                 } 
 
             }            
 
-            System.out.print("ODD DIGIT IN A NUMBER  : " + count);
+            System.out.print("NUMBER OF DIGIT DIVISIBLE BY 3 IS: " + count);
 
             sc.close();
 
