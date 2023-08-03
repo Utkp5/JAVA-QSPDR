993af5a5cca5fa14f5b7f0f20cd7ae4352c7ceba
PanditUtkarsh <utkp200@gmail.com>
2023-08-03 21:31:29 +0530

    loop ques

---
+++ Loop_Ques/Factors_of_number.java
@@ -0,0 +1,27 @@
+package Loop_Ques;
+import java.util.Scanner;
+
+// Question :-  WAP TO FIND THE FACTTORS OF A NUMBER 
+//like eg(6 :- 1,2,3,6)
+
+
+public class Factors_of_number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("ENTER A NUMBER : "); 
+        int num = sc.nextInt();
+        
+        int i = 1;
+
+        while (num >= i) {
+            
+            if (num%i==0) {
+                System.out.print(i + " ");
+            }
+            i++;
+        }
+
+        sc.close();    
+    }
+}
